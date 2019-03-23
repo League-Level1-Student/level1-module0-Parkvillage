e9e0308c08b04eb416729e3fcbcbae54c8b29821
league <league@worthymini-6.attlocal.net>
2019-03-23 11:26:46 -0700

    did stufff

---
+++ src/SoundEffects.java
@@ -0,0 +1,19 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SoundEffects {
+	public static void main(String[] args) {
+
+	}
+
+	void soundeffect() {
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		JButton button = new JButton();
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(button);
+	}
+
+}
